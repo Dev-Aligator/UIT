705994d8dafd3e040c468f96719bc4423779318e
Dev-Aligator <devaligator1004@gmail.com>
2024-06-17 21:32:35 +0700

    use kaggle input to load model weights

--- CS410.O21.KHCL/Assignments/BT6_TransformerModel/src/config.py
+++ CS410.O21.KHCL/Assignments/BT6_TransformerModel/src/config.py
@@ -8,7 +8,7 @@ def get_config():
         "seq_len": 80,
         "d_model": 512,
         "datasource": 'harouzie/vi_en-translation',
-        "modelsource": "/kaggle/input/nhtan-lab06-weights",
+        "modelsource": "/kaggle/input/model-weights-lab6",
         "lang_src": "English",
         "lang_tgt": "Vietnamese",
         "model_folder": "weights",
