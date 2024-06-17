f7c1ab0a474035aaf9dfbab849c1b7ea51bc72e2
Dev-Aligator <devaligator1004@gmail.com>
2024-06-17 21:37:54 +0700

    switch to item from item['translation']

--- CS410.O21.KHCL/Assignments/BT6_TransformerModel/src/train.py
+++ CS410.O21.KHCL/Assignments/BT6_TransformerModel/src/train.py
@@ -159,8 +159,8 @@ def get_ds(config):
     max_len_tgt = 0
 
     for item in ds_raw:
-        src_ids = tokenizer_src.encode(item['translation'][config['lang_src']]).ids
-        tgt_ids = tokenizer_tgt.encode(item['translation'][config['lang_tgt']]).ids
+        src_ids = tokenizer_src.encode(item[config['lang_src']]).ids
+        tgt_ids = tokenizer_tgt.encode(item[config['lang_tgt']]).ids
         max_len_src = max(max_len_src, len(src_ids))
         max_len_tgt = max(max_len_tgt, len(tgt_ids))
 
